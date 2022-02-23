ce788b448f5364a0d88a8e06bc19457e26ac73b7
Jan Janech <jan.janech@kst.uniza.sk>
2022-02-23 10:28:22 +0100

    01b - projekt pre pozicovnu vzducholodi

---
+++ pozicovnaVzducholodi/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: janik
+ * Date: 23. 2. 2022
+ * Time: 10:25
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO Write your code
+        System.out.println("Hello world");
+    }
+}
