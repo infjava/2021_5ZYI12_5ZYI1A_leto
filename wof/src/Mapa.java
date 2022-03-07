5126757aebcadbf1386c3a300a08f4be73899c88
Jan Janech <jan.janech@fri.uniza.sk>
2022-03-07 18:43:41 +0100

    03a - nova mapa hry - implementacia

--- wof/src/Mapa.java
+++ wof/src/Mapa.java
@@ -11,23 +11,57 @@ public class Mapa {
      */
     public Mapa() {
         // vytvorenie miestnosti
-        Miestnost terasa = new Miestnost("terasa - hlavny vstup na fakultu");
-        Miestnost aula = new Miestnost("aula");
-        Miestnost bufet = new Miestnost("bufet");
-        Miestnost labak = new Miestnost("pocitacove laboratorium");
-        Miestnost kancelaria = new Miestnost("kancelaria spravcu pocitacoveho laboratoria");
+        Miestnost internaty = new Miestnost("internaty - tu som doma");
+        Miestnost cesta = new Miestnost("cesta - hej hou, hej hou, uz ideme ... na fri");
+        Miestnost telocvicna = new Miestnost("telocvicna - divno to tu pachne, niekto sa zabudol osprchovat");
+        Miestnost schody = new Miestnost("schody - uz sa tesim do skoly");
+        Miestnost chodba = new Miestnost("chodba - vstupna chodba");
+        Miestnost vratnica = new Miestnost("vratnica - tu byva pani vratnicka");
+        Miestnost b = new Miestnost("b - temna chodba v budove b");
+        Miestnost b2 = new Miestnost("b2 - ako na dovolenke, pocut sum ... pocitacov");
+        Miestnost a = new Miestnost("a - chodba plna historickych artefaktov");
+        Miestnost c = new Miestnost("c - chodba v podzemi");
+        Miestnost a2 = new Miestnost("a2 - kniznica, tu treba byt ticho");
+        Miestnost wc = new Miestnost("wc - radsej ani nejdem hovorit, co tu vidim");
 
         // inicializacia miestnosti = nastavenie vychodov
-        terasa.nastavVychod("vychod", aula);
-        terasa.nastavVychod("juh", labak);
-        terasa.nastavVychod("zapad", bufet);
-        aula.nastavVychod("zapad", terasa);
-        bufet.nastavVychod("vychod", terasa);
-        labak.nastavVychod("sever", terasa);
-        labak.nastavVychod("vychod", kancelaria);
-        kancelaria.nastavVychod("zapad", labak);
+        internaty.nastavVychod("cesta", cesta);
 
-        this.startovaciaMiestnost = terasa;
+        cesta.nastavVychod("internaty", internaty);
+        cesta.nastavVychod("telocvicna", telocvicna);
+        cesta.nastavVychod("c", c);
+        cesta.nastavVychod("schody", schody);
+
+        telocvicna.nastavVychod("cesta", cesta);
+
+        schody.nastavVychod("cesta", cesta);
+        schody.nastavVychod("chodba", chodba);
+
+        chodba.nastavVychod("schody", schody);
+        chodba.nastavVychod("vratnica", vratnica);
+        chodba.nastavVychod("a", a);
+        chodba.nastavVychod("b", b);
+
+        b.nastavVychod("b2", b2);
+        b.nastavVychod("chodba", chodba);
+
+        b2.nastavVychod("b", b);
+
+        vratnica.nastavVychod("chodba", chodba);
+
+        a.nastavVychod("chodba", chodba);
+        a.nastavVychod("a2", a2);
+        a.nastavVychod("wc", wc);
+        a.nastavVychod("c", c);
+
+        a2.nastavVychod("a", a);
+
+        wc.nastavVychod("a", a);
+
+        c.nastavVychod("a", a);
+        c.nastavVychod("cesta", cesta);
+
+        this.startovaciaMiestnost = internaty;
     }
 
     public Miestnost getStartovaciaMiestnost() {
