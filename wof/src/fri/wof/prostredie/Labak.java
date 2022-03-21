d6b930d8efbdb2ff3d4abe2e77abdb5ecd20656a
Jan Janech <jan.janech@fri.uniza.sk>
2022-03-21 17:41:48 +0100

    05a - labak pomocou dedicnosti

--- wof/src/fri/wof/prostredie/Labak.java
+++ wof/src/fri/wof/prostredie/Labak.java
@@ -1,38 +1,13 @@
 package fri.wof.prostredie;
 
 import fri.wof.hra.Hrac;
-import fri.wof.predmety.IPredmet;
 import fri.wof.predmety.Navleky;
 
-public class Labak implements IMiestnost, IKontrolaVstupu {
-    private final IMiestnost vychod;
+public class Labak extends Miestnost implements IKontrolaVstupu {
+    public Labak(Miestnost von) {
+        super("Labak - miestnost plna pocitacov");
 
-    public Labak(IMiestnost vychod) {
-        this.vychod = vychod;
-    }
-
-    @Override
-    public void vypisMiestnost() {
-        System.out.println("Si v labaku");
-        System.out.println("Vychody: von");
-    }
-
-    @Override
-    public IMiestnost getMiestnostVSmere(String smer) {
-        if (smer.equals("von")) {
-            return this.vychod;
-        }
-        return null;
-    }
-
-    @Override
-    public void vlozPredmet(IPredmet predmet) {
-        System.out.printf("Automaticka ochrana laboratoria laserovym delom odstranila predmet %s%n", predmet.getNazov());
-    }
-
-    @Override
-    public IPredmet odstranPredmet(String nazovPredmetu) {
-        return null;
+        this.nastavVychod("von", von);
     }
 
     @Override
