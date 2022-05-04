20bf3b85fc839d448ef2ee86f7f75ef9f8783c8f
Jan Janech <jan.janech@fri.uniza.sk>
2022-05-04 11:00:09 +0200

    11b - hlavne okno cez dedicnost

--- vtipnaAppka/src/fri/vtipnaAppka/Main.java
+++ vtipnaAppka/src/fri/vtipnaAppka/Main.java
@@ -1,8 +1,11 @@
 package fri.vtipnaAppka;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        VtipneOkno okno = new VtipneOkno();
+        okno.zobraz();
     }
 }
---
+++ vtipnaAppka/src/fri/vtipnaAppka/VtipneOkno.java
@@ -0,0 +1,13 @@
+package fri.vtipnaAppka;
+
+import javax.swing.*;
+
+public class VtipneOkno extends JFrame {
+    public VtipneOkno() {
+        super("Vtipna aplikacia");
+    }
+
+    public void zobraz() {
+        this.setVisible(true);
+    }
+}
