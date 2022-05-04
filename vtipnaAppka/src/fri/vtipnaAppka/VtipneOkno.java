f0d480253ba334703a19b8511a921282fc85e4e3
Jan Janech <jan.janech@fri.uniza.sk>
2022-05-04 11:08:49 +0200

    11b - nie uplne dokonaly layout okna

--- vtipnaAppka/src/fri/vtipnaAppka/VtipneOkno.java
+++ vtipnaAppka/src/fri/vtipnaAppka/VtipneOkno.java
@@ -1,6 +1,7 @@
 package fri.vtipnaAppka;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class VtipneOkno {
     private final JFrame okno;
@@ -8,6 +9,9 @@ public class VtipneOkno {
     public VtipneOkno() {
         this.okno = new JFrame("Vtipna aplikacia");
         this.okno.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.okno.add(new JLabel("Chces ukoncit uspesne predmet informatika?"), BorderLayout.NORTH);
+        this.okno.add(new JButton("Ano"), BorderLayout.WEST);
+        this.okno.add(new JButton("Nie"), BorderLayout.EAST);
         this.okno.pack();
     }
 
