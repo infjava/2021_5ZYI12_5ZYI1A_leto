1cf439fd5a5b659d4a9d4242c888d2d2d6c6e82d
Jan Janech <jan.janech@fri.uniza.sk>
2022-05-09 17:50:35 +0200

    12a - odstranenie duplicit

--- vtipnaAppka/src/fri/vtipnaAppka/VtipneOkno.java
+++ vtipnaAppka/src/fri/vtipnaAppka/VtipneOkno.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 public class VtipneOkno {
     private final JFrame okno;
@@ -16,9 +15,20 @@ public class VtipneOkno {
         JPanel tlacidla = new JPanel();
         tlacidla.setLayout(new GridLayout());
 
-        JButton anoTlacitko = new JButton("Ano");
-        JButton nieTlacitko = new JButton("Nie");
+        JButton laveTlacitko = new JButton("Ano");
+        JButton praveTlacitko = new JButton("Nie");
 
+        pridajListenery(laveTlacitko, praveTlacitko);
+        pridajListenery(praveTlacitko, laveTlacitko);
+
+        tlacidla.add(laveTlacitko);
+        tlacidla.add(praveTlacitko);
+
+        this.okno.add(tlacidla);
+        this.okno.pack();
+    }
+
+    private void pridajListenery(JButton anoTlacitko, JButton nieTlacitko) {
         anoTlacitko.addActionListener(e -> {
             JOptionPane.showMessageDialog(null, "Tak by si sa mal zacat snazit");
             System.exit(0);
@@ -30,24 +40,6 @@ public class VtipneOkno {
                 nieTlacitko.setText("Nie");
             }
         });
-
-        nieTlacitko.addActionListener(e -> {
-            JOptionPane.showMessageDialog(null, "Tak by si sa mal zacat snazit");
-            System.exit(0);
-        });
-        nieTlacitko.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                anoTlacitko.setText("Nie");
-                nieTlacitko.setText("Ano");
-            }
-        });
-
-        tlacidla.add(anoTlacitko);
-        tlacidla.add(nieTlacitko);
-
-        this.okno.add(tlacidla);
-        this.okno.pack();
     }
 
     public void zobraz() {
