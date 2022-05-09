d4e3a532d54fd8b151a842e869ed6ba268ffb073
Jan Janech <jan.janech@fri.uniza.sk>
2022-05-09 18:27:03 +0200

    12a - zobrazenie okna so zoznamom studentov

--- intraky/src/fri/intraky/HlavneOkno.java
+++ intraky/src/fri/intraky/HlavneOkno.java
@@ -1,4 +1,16 @@
 package fri.intraky;
 
+import javax.swing.*;
+
 public class HlavneOkno {
+    private JList list1;
+    private JPanel panel1;
+
+    public void zobraz() {
+        JFrame okno = new JFrame("Intraky IS");
+        okno.add(this.panel1);
+        okno.pack();
+        okno.setVisible(true);
+    }
+
 }
--- intraky/src/fri/intraky/Main.java
+++ intraky/src/fri/intraky/Main.java
@@ -3,6 +3,7 @@ package fri.intraky;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
+	    HlavneOkno okno = new HlavneOkno();
+        okno.zobraz();
     }
 }
