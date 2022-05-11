bc9efd6fbc801679cbfd1cd8929f8c2328d211dd
Jan Janech <jan.janech@fri.uniza.sk>
2022-05-11 10:49:54 +0200

    12b - automaticke povolovanie tlacitka Pridaj

--- intraky/src/fri/intraky/HlavneOkno.java
+++ intraky/src/fri/intraky/HlavneOkno.java
@@ -1,6 +1,8 @@
 package fri.intraky;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -20,6 +22,35 @@ public class HlavneOkno {
         this.zoznamStudentov = new DefaultListModel<UbytovanyStudent>();
 
         pridajButton.addActionListener(e -> this.pridajStudenta());
+
+        DocumentListener reakciaNaZmenu = new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                HlavneOkno.this.aktualizujOpravnenia();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                HlavneOkno.this.aktualizujOpravnenia();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                HlavneOkno.this.aktualizujOpravnenia();
+            }
+        };
+
+        this.menoTextField.getDocument().addDocumentListener(reakciaNaZmenu);
+        this.priezviskoTextField.getDocument().addDocumentListener(reakciaNaZmenu);
+        this.izbaTextField.getDocument().addDocumentListener(reakciaNaZmenu);
+
+        this.aktualizujOpravnenia();
+    }
+
+    private void aktualizujOpravnenia() {
+        this.pridajButton.setEnabled(!this.menoTextField.getText().isEmpty()
+                && !this.priezviskoTextField.getText().isEmpty()
+                && !this.izbaTextField.getText().isEmpty());
     }
 
     private void pridajStudenta() {
